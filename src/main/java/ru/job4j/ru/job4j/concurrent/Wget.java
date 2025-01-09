57b14027ace7444741f71704446523bba6c7199c
Vasiliy Krasov <v.krasov.ext@altrnativ.com>
2025-01-09 21:54:14 +0100

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,17 @@
+package ru.job4j.ru.job4j.concurrent;
+
+public class Wget {
+  public static void main(String[] args) {
+    Thread loader = new Thread(() -> {
+      try {
+        for (int index = 0; index < 101; index++) {
+          System.out.print("\rLoading : " + index  + "%");
+          Thread.sleep(1000);
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    });
+    loader.start();
+  }
+}
