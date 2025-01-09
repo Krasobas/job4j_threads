cd78d85f4f1d7cf6b7ab64b43515d5aee6db6c2f
Vasiliy Krasov <v.krasov.ext@altrnativ.com>
2025-01-09 22:43:38 +0100

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j.ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+  @Override
+  public void run() {
+    int count = 0;
+    var process = new char[] {'-', '\\', '|', '/'};
+    while (!Thread.currentThread().isInterrupted()) {
+      if (count == process.length) {
+        count = 0;
+      }
+      System.out.print("\r Loading ... " + process[count++]);
+    }
+  }
+
+  public static void main(String[] args) {
+    Thread progress = new Thread(new ConsoleProgress());
+    progress.start();
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    progress.interrupt();
+  }
+}
