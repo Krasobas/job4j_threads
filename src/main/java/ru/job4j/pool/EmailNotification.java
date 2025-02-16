3adfeb1d196b88770365054570148a2062e1afd3
Vasiliy Krasov <v.krasov.ext@altrnativ.com>
2025-02-16 22:00:08 +0100

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,28 @@
+package ru.job4j.pool;
+
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+  ExecutorService pool = Executors.newCachedThreadPool();
+
+  public void send(String subject, String body, String email) {
+
+  }
+
+  public void emailTo(User user) {
+    if (Objects.isNull(user)) {
+      return;
+    }
+    pool.submit(() -> {
+      String subject = String.format("Notification %s to email %s", user.username(), user.email());
+      String body = String.format("Add a new event to %s", user.username());
+      send(subject, body, user.email());
+    });
+  }
+
+  public void close() {
+    pool.shutdown();
+  }
+}
